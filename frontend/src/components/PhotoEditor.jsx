9b7ad5d0a6c2f305d59b0d10bc10a4ce22b018ba
Xanzik <pustovoi2005@gmail.com>
2024-05-26 18:55:08 +0300

    Frontend 0.1

--- frontend/src/components/PhotoEditor.jsx
+++ frontend/src/components/PhotoEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { fabric } from 'fabric';
 
@@ -7,6 +7,18 @@ import styles from '../styles/PhotoEditor.module.css';
 const PhotoEditor = ({ imageSettings }) => {
     const [imageLoaded, setImageLoaded] = useState(false);
     const canvasRef = useRef(null);
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        // Функция, которая будет вызываться после каждого рендера
+        // Проверяем, есть ли ссылка на контейнер, и если есть, выводим его размеры
+        if (containerRef.current) {
+            console.log(
+                containerRef.current.offsetWidth,
+                containerRef.current.offsetHeight,
+            );
+        }
+    });
 
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
@@ -15,17 +27,34 @@ const PhotoEditor = ({ imageSettings }) => {
             const imgData = e.target.result;
             fabric.Image.fromURL(imgData, (img) => {
                 const canvas = new fabric.Canvas(canvasRef.current);
+                canvas.setDimensions({
+                    width: containerRef.current.offsetWidth,
+                    height: containerRef.current.offsetHeight,
+                });
+
                 canvas.clear();
+                const scaleFactor = Math.min(
+                    canvas.width / img.width,
+                    canvas.height / img.height,
+                );
                 img.set({
-                    scaleX: canvas.width / img.width,
-                    scaleY: canvas.height / img.height,
+                    scaleX: scaleFactor,
+                    scaleY: scaleFactor,
+                });
+                img.set({
+                    maxWidth: canvas.width,
+                    maxHeight: canvas.height,
+                });
+                img.set({
+                    top: (canvas.height - img.height * scaleFactor) / 2,
+                    left: (canvas.width - img.width * scaleFactor) / 2,
+                    selectable: false, // Предотвращаем выбор изображения, чтобы пользователь не мог его передвигать
                 });
                 canvas.add(img);
                 canvas.renderAll();
             });
         };
         setImageLoaded(true);
-        console.log(file);
         reader.readAsDataURL(file);
     };
 
@@ -44,23 +73,24 @@ const PhotoEditor = ({ imageSettings }) => {
     // };
 
     return (
-        <div className={styles.photoEditorContainer}>
-            <Container className="d-flex justify-content-center align-items-center h-100">
-                {imageLoaded ? null : (
-                    <label htmlFor="imageInput" className={styles.imageInput}>
-                        Choose Image
-                        <input
-                            id="imageInput"
-                            type="file"
-                            accept="image/*"
-                            className="d-none"
-                            onChange={handleImageUpload}
-                        />
-                    </label>
-                )}
-                {imageLoaded && <canvas ref={canvasRef} />}
-            </Container>
-        </div>
+        <Container
+            ref={containerRef}
+            className={`d-flex justify-content-center align-items-center ${styles.canvasContainer}`}
+        >
+            {imageLoaded ? null : (
+                <label htmlFor="imageInput" className={styles.imageInput}>
+                    Choose Image
+                    <input
+                        id="imageInput"
+                        type="file"
+                        accept="image/*"
+                        className="d-none"
+                        onChange={handleImageUpload}
+                    />
+                </label>
+            )}
+            {imageLoaded && <canvas ref={canvasRef}></canvas>}
+        </Container>
     );
 };
 
